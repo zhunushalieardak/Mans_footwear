908b8440f2e0f779b1666acdbb214496a3b00e5c
zhunushalieardak <zhunushalieardak@gmail.com>
2024-05-29 14:11:04 +0600

    About.js

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,6 +6,7 @@ function About() {
       <div className="container">
         <div className='About_txt'>
         <h1>"Welcome to our online men's shoe store! We are proud to offer high quality shoes for the modern man who values comfort, style and quality. Our mission is to help you look stylish and confident in any situation. We carefully select shoes from the best manufacturers to provide you with the best selection. Whether you are looking for classic office shoes, comfortable sneakers for an active lifestyle, or elegant boots for special occasions, we have what you need. Join us and emphasize your style with our shoes!</h1>
+        <h2>"Welcome to our online men's shoe store! We are proud to offer high quality shoes for the modern man who values comfort, style and quality. Our mission is to help you look stylish and confident in any situation. We carefully select shoes from the best manufacturers to provide you with the best selection. Whether you are looking for classic office shoes, comfortable sneakers for an active lifestyle, or elegant boots for special occasions, we have what you need. Join us and emphasize your style with our shoes!</h2>
         </div>
       </div>
     </section>
